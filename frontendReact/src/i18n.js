a5dcc683e77c3d0fc7252e016c96f02e54971af2
Rajib3003 <115511670+Rajib3003@users.noreply.github.com>
2024-11-11 17:39:02 +0600

    default banglay langauage show

--- frontendReact/src/i18n.js
+++ frontendReact/src/i18n.js
@@ -3,6 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import enTranslations from './locales/en/common.json';
 import bnTranslations from './locales/bn/common.json';
 
+const language = localStorage.getItem('language') || 'bn';
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
@@ -14,11 +15,14 @@ i18n
         translation: bnTranslations,
       },
     },
-    lng: 'en', // default language
+    lng: language, // default language
     fallbackLng: 'en', // fallback language if the translation is missing in the selected language
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
     },
+  });
+  i18n.on('languageChanged', (lng) => {
+    localStorage.setItem('language', lng);
   });
 
 export default i18n;
