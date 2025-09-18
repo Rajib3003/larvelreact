a0c27b04ef5967b0b4cc8f7802d31fac23e5a739
rajibbhuiyan-afk <rajib.bhuiyan@biometrics-bd.com>
2025-09-18 17:22:07 +0600

    teacher information add

--- frontendReact/src/pages/teachers/TeacherList.jsx
+++ frontendReact/src/pages/teachers/TeacherList.jsx
@@ -1,41 +1,42 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 // Placeholder images for teachers
-import classesImage1 from '/assets/frontend_assets/img/play-1.png';
-import classesImage2 from '/assets/frontend_assets/img/one-1.png';
-import classesImage3 from '/assets/frontend_assets/img/classes-3.jpg';
-import classesImage4 from '/assets/frontend_assets/img/classes-4.jpg';
-import classesImage5 from '/assets/frontend_assets/img/classes-5.jpg';
-import classesImage6 from '/assets/frontend_assets/img/classes-6.jpg';
+import aloRaniSaha from '/assets/frontend_assets/img/teachers/alo-rani-saha.png';
+import kabiMowshumiMow from '/assets/frontend_assets/img/teachers/kabi-mowshumi-mow.png';
+import Mansura from '/assets/frontend_assets/img/teachers/mansura.png';
+import MituRaniHalder from '/assets/frontend_assets/img/teachers/mitu-rani-halder.png';
+import nasimaKhan from '/assets/frontend_assets/img/teachers/nasima-khan.png';
+import nironjonaGhoshProma from '/assets/frontend_assets/img/teachers/nironjona-ghosh-proma.png';
+import rabeaAlamgir from '/assets/frontend_assets/img/teachers/rabea-alamgir.png';
 
 export default function TeacherList() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  // const [posts, setPosts] = useState([]);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    // Fetch data from the API
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((data) => {
-        setPosts(data); // Limit to 6 posts for display
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
-  }, []);
+  // useEffect(() => {
+    
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setPosts(data); 
+  //       setLoading(false);
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error);
+  //       setLoading(false);
+  //     });
+  // }, []);
 
   // Placeholder images array
-  const images = [
-    classesImage1,
-    classesImage2,
-    classesImage3,
-    classesImage4,
-    classesImage5,
-    classesImage6,
-    classesImage1,
-  ];
+  // const images = [
+  //   classesImage1,
+  //   classesImage2,
+  //   classesImage3,
+  //   classesImage4,
+  //   classesImage5,
+    // classesImage6,
+  //   classesImage1,
+  // ];
 
   return (
     <div className="container-xxl py-5">
@@ -44,7 +45,7 @@ export default function TeacherList() {
           <h1 className="mb-3">All Teachers List</h1>
           <p>Eirmod sed ipsum dolor sit rebum labore magna erat. Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
         </div>
-        {loading ? (
+        {/* {loading ? (
           <p>Loading...</p>
         ) : (
           <div className="row g-4">
@@ -67,15 +68,173 @@ export default function TeacherList() {
                           >
                             Read More
                           </button>
-                        </div>
-                      {/* </p> */}
+                        </div>                      
                     </div>
                   </div>
                 </div>
               </div>
             ))}
           </div>
-        )}
+        )} */}
+
+
+          <div className="row g-4">
+          
+              <div  className={`col-lg-4 col-md-6 wow fadeInUp`} data-wow-delay={`${0.01 * 0}s`}>
+                <div className="classes-item">
+                  <div className="bg-light rounded-circle w-50 mx-auto p-3">
+                    <img className="img-fluid rounded-circle" src={aloRaniSaha} alt="Teacher" />
+                  </div>
+                  <div className="bg-light rounded p-4 pt-3 mt-n5">
+                    <a className="d-block text-center h3 mt-3 " href="#">Alo Rani Saha</a>
+                    <p className="d-block text-center">Drawing Teacher</p>
+                    <div className="row g-1">
+                      <p></p>
+                        <div className="d-flex justify-content-end">
+                          <button
+                            className="btn btn-primary ms-2"
+                            onClick={() => window.open(`${import.meta.env.BASE_URL}/teacherdetails/1`, '_self')}
+                          >
+                            Read More
+                          </button>
+                        </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>         
+              <div  className={`col-lg-4 col-md-6 wow fadeInUp`} data-wow-delay={`${0.01 * 0}s`}>
+                <div className="classes-item">
+                  <div className="bg-light rounded-circle w-50 mx-auto p-3">
+                    <img className="img-fluid rounded-circle" src={kabiMowshumiMow} alt="Teacher" />
+                  </div>
+                  <div className="bg-light rounded p-4 pt-3 mt-n5">
+                    <a className="d-block text-center h3 mt-3 " href="#">Kabi Mowshumi Mow</a>
+                    <p className="d-block text-center">Music Teacher</p>
+                    <div className="row g-1">
+                      <p></p>
+                        <div className="d-flex justify-content-end">
+                          <button
+                            className="btn btn-primary ms-2"
+                            // onClick={() => window.open(`${import.meta.env.BASE_URL}/teacherdetails/${post.id}`, '_self')}
+                          >
+                            Read More
+                          </button>
+                        </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>         
+              <div  className={`col-lg-4 col-md-6 wow fadeInUp`} data-wow-delay={`${0.01 * 0}s`}>
+                <div className="classes-item">
+                  <div className="bg-light rounded-circle w-50 mx-auto p-3">
+                    <img className="img-fluid rounded-circle" src={Mansura} alt="Teacher" />
+                  </div>
+                  <div className="bg-light rounded p-4 pt-3 mt-n5">
+                    <a className="d-block text-center h3 mt-3 " href="#">Mansura</a>
+                    <p className="d-block text-center">Assistant Teacher</p>
+                    <div className="row g-1">
+                      <p></p>
+                        <div className="d-flex justify-content-end">
+                          <button
+                            className="btn btn-primary ms-2"
+                            // onClick={() => window.open(`${import.meta.env.BASE_URL}/teacherdetails/${post.id}`, '_self')}
+                          >
+                            Read More
+                          </button>
+                        </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>         
+              <div  className={`col-lg-4 col-md-6 wow fadeInUp`} data-wow-delay={`${0.01 * 0}s`}>
+                <div className="classes-item">
+                  <div className="bg-light rounded-circle w-50 mx-auto p-3">
+                    <img className="img-fluid rounded-circle" src={MituRaniHalder} alt="Teacher" />
+                  </div>
+                  <div className="bg-light rounded p-4 pt-3 mt-n5">
+                    <a className="d-block text-center h3 mt-3 " href="#">Mitu Rani Halder</a>
+                    <p className="d-block text-center">Senior Assistant Teacher</p>
+                    <div className="row g-1">
+                      <p></p>
+                        <div className="d-flex justify-content-end">
+                          <button
+                            className="btn btn-primary ms-2"
+                            // onClick={() => window.open(`${import.meta.env.BASE_URL}/teacherdetails/${post.id}`, '_self')}
+                          >
+                            Read More
+                          </button>
+                        </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>         
+              <div  className={`col-lg-4 col-md-6 wow fadeInUp`} data-wow-delay={`${0.01 * 0}s`}>
+                <div className="classes-item">
+                  <div className="bg-light rounded-circle w-50 mx-auto p-3">
+                    <img className="img-fluid rounded-circle" src={nasimaKhan} alt="Teacher" />
+                  </div>
+                  <div className="bg-light rounded p-4 pt-3 mt-n5">
+                    <a className="d-block text-center h3 mt-3 " href="#">Nasima Khan</a>
+                    <p className="d-block text-center">Assistant Teacher</p>
+                    <div className="row g-1">
+                      <p></p>
+                        <div className="d-flex justify-content-end">
+                          <button
+                            className="btn btn-primary ms-2"
+                            // onClick={() => window.open(`${import.meta.env.BASE_URL}/teacherdetails/${post.id}`, '_self')}
+                          >
+                            Read More
+                          </button>
+                        </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>         
+              <div  className={`col-lg-4 col-md-6 wow fadeInUp`} data-wow-delay={`${0.01 * 0}s`}>
+                <div className="classes-item">
+                  <div className="bg-light rounded-circle w-50 mx-auto p-3">
+                    <img className="img-fluid rounded-circle" src={nironjonaGhoshProma} alt="Teacher" />
+                  </div>
+                  <div className="bg-light rounded p-4 pt-3 mt-n5">
+                    <a className="d-block text-center h3 mt-3 " href="#">Nironjona Ghosh Proma</a>
+                    <p className="d-block text-center">Assistant Teacher</p>
+                    <div className="row g-1">
+                      <p></p>
+                        <div className="d-flex justify-content-end">
+                          <button
+                            className="btn btn-primary ms-2"
+                            // onClick={() => window.open(`${import.meta.env.BASE_URL}/teacherdetails/${post.id}`, '_self')}
+                          >
+                            Read More
+                          </button>
+                        </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>         
+              <div  className={`col-lg-4 col-md-6 wow fadeInUp`} data-wow-delay={`${0.01 * 0}s`}>
+                <div className="classes-item">
+                  <div className="bg-light rounded-circle w-50 mx-auto p-3">
+                    <img className="img-fluid rounded-circle" src={rabeaAlamgir} alt="Teacher" />
+                  </div>
+                  <div className="bg-light rounded p-4 pt-3 mt-n5">
+                    <a className="d-block text-center h3 mt-3 " href="#">Rabea Alamgir</a>
+                    <p className="d-block text-center">Assistant Teacher</p>
+                    <div className="row g-1">
+                      <p></p>
+                        <div className="d-flex justify-content-end">
+                          <button
+                            className="btn btn-primary ms-2"
+                            // onClick={() => window.open(`${import.meta.env.BASE_URL}/teacherdetails/${post.id}`, '_self')}
+                          >
+                            Read More
+                          </button>
+                        </div>                      
+                    </div>
+                  </div>
+                </div>
+              </div>         
+          </div>      
       </div>
     </div>
   );
