d32b0327e09dc7622fa306e23371de164e540ca6
Rajib3003 <115511670+Rajib3003@users.noreply.github.com>
2025-09-20 15:11:46 +0600

    class section update

--- frontendReact/src/pages/classes/Classes.jsx
+++ frontendReact/src/pages/classes/Classes.jsx
@@ -22,15 +22,70 @@ export default function Classes() {
 
           <div className="row g-4">
             
-            <SingleClass image={teamImageOne} classTitle={t("class-playgroup")} age={t("5-6-years")} time={t("9-10-am")} capacity={t("30-kids")} />         
-            <SingleClass image={teamImageOne} classTitle={t("class-nursery")} age={t("5-6-years")} time={t("9-10-am")} capacity={t("30-kids")} />         
-            <SingleClass image={teamImageOne} classTitle={t("class-lkg")} age={t("5-6-years")} time={t("9-10-am")} capacity={t("30-kids")} />         
-            <SingleClass image={teamImageOne} classTitle={t("class-ukg")} age={t("5-6-years")} time={t("9-10-am")} capacity={t("30-kids")} />         
-            <SingleClass image={teamImageOne} classTitle={t("class-one")} age={t("5-6-years")} time={t("9-10-am")} capacity={t("30-kids")} />         
-            <SingleClass image={teamImageOne} classTitle={t("class-two")} age={t("5-6-years")} time={t("9-10-am")} capacity={t("30-kids")} />         
-            <SingleClass image={teamImageOne} classTitle={t("class-three")} age={t("5-6-years")} time={t("9-10-am")} capacity={t("30-kids")} />         
-            <SingleClass image={teamImageOne} classTitle={t("class-four")} age={t("5-6-years")} time={t("9-10-am")} capacity={t("30-kids")} />         
-            <SingleClass image={teamImageOne} classTitle={t("class-five")} age={t("5-6-years")} time={t("9-10-am")} capacity={t("30-kids")} />         
+            <SingleClass 
+              image={teamImageOne} 
+              classTitle={t("class-pre-play")} 
+              shift1={t("preplayshift1")}
+              shift2={t("preplayshift2")}
+              shift3={t("preplayshift3")}              
+              capacity={t("10-kids")} 
+            />         
+            <SingleClass 
+              image={teamImageOne} 
+              classTitle={t("class-play")} 
+              shift1={t("playshift1")}
+              shift2={t("playshift2")}
+              shift3={t("playshift3")} 
+              capacity={t("30-kids")} 
+            />         
+            <SingleClass 
+              image={teamImageOne} 
+              classTitle={t("class-nursery")} 
+              shift1={t("nursery1")}
+              shift2={t("nursery2")}
+              shift3={t("nursery3")} 
+              capacity={t("30-kids")} 
+            />         
+            <SingleClass 
+            image={teamImageOne} 
+            classTitle={t("class-kg")} 
+            shift1={t("kg1")}
+            shift2={t("kg2")}            
+            capacity={t("30-kids")} 
+            />                      
+            <SingleClass 
+            image={teamImageOne} 
+            classTitle={t("class-one")} 
+            shift1={t("kg1")}
+            shift2={t("kg2")}  
+            capacity={t("30-kids")} 
+            />         
+            <SingleClass 
+            image={teamImageOne} 
+            classTitle={t("class-two")} 
+            shift1={t("kg1")}
+            shift2={t("kg2")}   
+            capacity={t("30-kids")} 
+            />         
+            <SingleClass 
+            image={teamImageOne} 
+            classTitle={t("class-three")} 
+            shift1={t("kg1")}
+            shift2={t("kg2")}  
+            capacity={t("30-kids")} 
+            />         
+            <SingleClass 
+            image={teamImageOne} 
+            classTitle={t("class-four")} 
+            shift1={t("kg1")}             
+            capacity={t("30-kids")} 
+            />         
+            <SingleClass 
+            image={teamImageOne} 
+            classTitle={t("class-five")} 
+            shift1={t("kg1")}            
+            capacity={t("30-kids")} 
+            />         
                      
                      
           </div>
--- frontendReact/src/pages/classes/SingleClass.jsx
+++ frontendReact/src/pages/classes/SingleClass.jsx
@@ -3,36 +3,65 @@
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
- const  SingleClass = ({image, classTitle, age, time, capacity }) => {
+ const  SingleClass = ({image, classTitle,shift1,shift2,shift3, capacity }) => {
      const { t } = useTranslation();
   return (
     <div  className={`col-lg-4 col-md-6 wow fadeInUp`} data-wow-delay={`${0.1 * 0}s`}>
         <div className="classes-item">
             <div className="bg-light rounded-circle w-75 mx-auto p-3">
-            <img className="img-fluid rounded-circle" src={image} alt="" />
+                <img className="img-fluid rounded-circle" src={image} alt="" />
             </div>
             <div className="bg-light rounded p-4 pt-5 mt-n5">
-            <a className="d-block text-center h3 mt-3 mb-4" href=""> {classTitle}</a>                
-            <div className="row g-1">
-                <div className="col-4">
-                <div className="border-top border-3 border-primary pt-2">
-                    <h6 className="text-primary mb-1">{t("age")}:</h6>
-                    <small>{age}</small>
+                <a className="d-block text-center h3 mt-3 mb-4" href=""> {classTitle}</a>  
+                
+                <div className="mt-4">
+                    <div className="border-top border-3 border-primary pt-3 pb-2 d-flex justify-content-between align-items-center">
+                        <div>
+                            <h6 className="text-primary mb-1 d-inline">{t("shift")}</h6>                    
+                        </div>
+                        <div>
+                            <h6 className="text-primary mb-1 d-inline">{t("time")}</h6>                    
+                        </div>
+                        <div>
+                            <h6 className="text-primary mb-1 d-inline">{t("capacity")}</h6>                    
+                        </div>
+                    </div>
                 </div>
+                {shift1 && (
+                <div className="row g-1">
+                    <div className="col-12">
+                        <div className=" pt-2 d-flex justify-content-between align-items-center">
+                            <small>{t("morning-a")}</small>
+                            <small>{shift1}</small>
+                            <small>{capacity}</small>
+                        </div> 
+                    </div>
                 </div>
-                <div className="col-4">
-                <div className="border-top border-3 border-success pt-2">
-                    <h6 className="text-success mb-1">{t("time")}:</h6>
-                    <small>{time}</small>
+                )}
+                {shift2 && (
+                <div className="row g-1">
+                    <div className="col-12">
+                        <div className=" pt-2 d-flex justify-content-between align-items-center">
+                            <small>{t("morning-b")}</small>
+                            <small>{shift2}</small>
+                            <small>{capacity}</small>
+                        </div> 
+                    </div>
                 </div>
-                </div>
-                <div className="col-4">
-                <div className="border-top border-3 border-warning pt-2">
-                    <h6 className="text-warning mb-1">{t("capacity")}:</h6>
-                    <small>{capacity}</small>
-                </div>
-                </div>
-            </div>
+                )}
+                {shift3 && (
+                    <div className="row g-1">
+                        <div className="col-12">
+                        <div className="pt-2 d-flex justify-content-between align-items-center">
+                            <small>{t("day")}</small>
+                            <small>{shift3}</small>
+                            <small>{capacity}</small>
+                        </div>
+                        </div>
+                    </div>
+                )}
+
+                
             </div>
         </div>
     </div> 
@@ -40,11 +69,12 @@ import { useTranslation } from 'react-i18next';
 }
 
 SingleClass.propTypes = {
-  image: PropTypes.string.isRequired,
-  classTitle: PropTypes.string.isRequired,
-  age: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-  time: PropTypes.string.isRequired,
-  capacity: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    image: PropTypes.string.isRequired,
+    classTitle: PropTypes.string.isRequired,
+    shift1: PropTypes.string,
+    shift2: PropTypes.string,
+    shift3: PropTypes.string,   
+    capacity: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
 };
 
 export default SingleClass;
